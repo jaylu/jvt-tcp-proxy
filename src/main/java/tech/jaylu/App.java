eb05c794f9af4846d0ab5eb6fcf49c73554db3f6
Jay LU <jayjjlu@gmail.com>
2025-05-18 00:17:31 +0800

    use nio implementation

--- src/main/java/tech/jaylu/App.java
+++ src/main/java/tech/jaylu/App.java
@@ -5,10 +5,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -16,7 +16,7 @@ public class App {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
     private final Config config;
-    private final AtomicReference<ServerSocket> serverSocketRef = new AtomicReference<>();
+    private final AtomicReference<ServerSocketChannel> serverSocketRef = new AtomicReference<>();
     private boolean isStop = false;
 
     public App(Config config) {
@@ -25,17 +25,20 @@ public class App {
 
     public void start() throws IOException {
         log.info("tcp proxy server starting with config: {}", this.config);
-        ServerSocket serverSocket = new ServerSocket(config.getFromPort());
-        serverSocketRef.set(serverSocket);
-        Thread.startVirtualThread(() -> startServer(serverSocket));
-        log.info("tcp proxy server started at {}.", serverSocket.getLocalSocketAddress().toString());
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(config.getFromPort()));
+
+        serverSocketRef.set(serverSocketChannel);
+        Thread.startVirtualThread(() -> startServer(serverSocketChannel));
+        log.info("tcp proxy server started at {}.", serverSocketChannel.socket().getLocalSocketAddress().toString());
     }
 
-    private void startServer(ServerSocket serverSocket) {
+    private void startServer(ServerSocketChannel serverSocketChannel) {
         while (!isStop) {
             try {
-                final Socket client = serverSocket.accept();
-                Thread.startVirtualThread(() -> handleClientSocket(client));
+                final SocketChannel clientChannel = serverSocketChannel.accept();
+                Thread.startVirtualThread(() -> handleClientSocket(clientChannel));
             } catch (IOException e) {
                 // TODO: maybe need to stop the server have issue on accepting new connection ?
                 log.info("failed to accept new connection: {}", e.getMessage());
@@ -44,37 +47,36 @@ public class App {
         }
     }
 
-    private void handleClientSocket(Socket client) {
-        try (Socket target = new Socket("localhost", config.getToPort())) {
+    private void handleClientSocket(SocketChannel clientChannel) {
+        InetSocketAddress targetAddress = new InetSocketAddress("localhost", config.getToPort());
+        try (SocketChannel targetChannel = SocketChannel.open(targetAddress)) {
             CountDownLatch latch = new CountDownLatch(2);
             Thread.startVirtualThread(() -> {
-                pipe(client, target, "Client -> Target");
+                pipe(clientChannel, targetChannel, "Client -> Target");
                 latch.countDown();
             });
             Thread.startVirtualThread(() -> {
-                pipe(target, client, "Target -> Client");
+                pipe(targetChannel, clientChannel, "Target -> Client");
                 latch.countDown();
             });
             latch.await();
         } catch (Exception e) {
             log.info("Error connecting to target socket: {}", e.getMessage());
         } finally {
-            safeClose(client);
+            safeClose(clientChannel);
         }
     }
 
-    private void pipe(Socket from, Socket to, String label) {
-        byte[] buffer = new byte[8192];
-        try  {
-            InputStream input = from.getInputStream();
-            OutputStream output = to.getOutputStream();
-            int readCount;
-            while ((readCount = input.read(buffer)) != -1) {
-                output.write(buffer, 0, readCount);
-                output.flush();
+    private void pipe(SocketChannel fromChannel, SocketChannel toChannel, String label) {
+        try {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(8192);
+            while (fromChannel.read(buffer) != -1) {
+                buffer.flip();
+                toChannel.write(buffer);
+                buffer.clear();
             }
         } catch (Exception e) {
-            log.info(label + "  failed to pipe data stream", e);
+            log.info(label + " failed to pipe data stream", e);
         }
     }
 
